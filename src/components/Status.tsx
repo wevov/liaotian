32d3666eaaaf766f8fd8dcbe1bb6197936e7f7fa
Cooper Black's <cooperblackdboss@gmail.com>
2025-11-17 00:39:40 +0600

    Update Status.tsx

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,6 +1,6 @@
 // src/components/Status.tsx
 import React, { useState, useEffect, useRef, useCallback, Fragment } from 'react';
-import { supabase, uploadStatusMedia, Profile, Status as StatusType } from '../lib/supabase';
+import { supabase, uploadStatusMedia, Profile, BadgeCheck, Status as StatusType } from '../lib/supabase';
 import { useAuth } from '../contexts/AuthContext';
 import { 
   X, 
